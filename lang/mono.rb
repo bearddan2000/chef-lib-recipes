0f584945d06f2431f66f6fd067b4be64f15d72a6
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2020-11-26 22:44:36 -0500

    Update mono.rb

--- lang/mono.rb
+++ lang/mono.rb
@@ -5,8 +5,12 @@ package 'dirmngr'
 package 'gpg-agent'
 package 'software-properties-common'
 
+execute 'apt-key' do
+  command 'apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF'
+end
+
 execute 'apt-update' do
-  command 'sudo apt-get update --fix-missing'
+  command 'apt-get update --fix-missing'
 end
 
 package 'mono-complete'
